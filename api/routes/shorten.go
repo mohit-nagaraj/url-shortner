1380018c428a44b85e17889bd8db0618d90c371f
mohit-nagaraj <mohitnagaraj20@gmail.com>
2024-06-19 19:56:04 +0530

    add the helper functions

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -1,6 +1,12 @@
 package routes
 
-import "time"
+import (
+	"time"
+
+	"github.com/asaskevich/govalidator"
+	"github.com/gofiber/fiber/v3"
+	"github.com/mohit-nagaraj/url-shortner/helpers"
+)
 
 // define the structure of the request so get to kno
 type request struct {
@@ -15,4 +21,38 @@ type response struct {
 	Expiry         time.Duration `json:"expiry"`
 	XRateRemaining int           `json:"rate_limit"`
 	XRateLimitRest time.Duration `json:"rate_limit_reset"`
+}
+
+func ShortenURL(c *fiber.Ctx) error {
+	// check for the incoming request body
+	body := new(request)
+
+	if err := c.BodyParser(&body); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "cannot parse JSON",
+		})
+	}
+
+	// implement rate limiting
+
+	// check if the input is an actual URL
+	if !govalidator.IsURL(body.URL) {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "Invalid URL",
+		})
+	}
+
+	// check for the domain error
+	// users may abuse the shortener by shorting the domain `localhost:3000` itself
+	// leading to a inifite loop, so don't accept the domain for shortening
+	if !helpers.RemoveDomainError(body.URL) {
+		return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{
+			"error": "haha... nice try",
+		})
+	}
+
+	// enforce https
+	// all url will be converted to https before storing in database
+	body.URL = helpers.EnforceHTTP(body.URL)
+
 }
